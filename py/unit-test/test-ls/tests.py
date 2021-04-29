ae483295ae002a64306a5b2d58fb335184107b5b
sameer <sameer.barha12@gmail.com>
2021-04-29 15:14:33 +0530

    added tests for hidden files

--- py/unit-test/test-ls/tests.py
+++ py/unit-test/test-ls/tests.py
@@ -26,3 +26,30 @@ class ClassTest(object):
             assert 'first.txt' in str(result.stdout), "Error while listing a folder with one file !"
         finally:
             shutil.rmtree('/tmp/testfolder')
+
+    @staticmethod
+    def test_list_multiple_files():
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            Path("/tmp/testfolder/second.doc").touch()
+            result = subprocess.run(['ls', '/tmp/testfolder'], stdout=subprocess.PIPE)
+            print('Result: [{}]'.format(result))
+            assert 'first.txt' in str(result.stdout), "Error while listing a folder with multiple !"
+            assert 'second.doc' in str(result.stdout), "Error while listing a folder with multiple !"
+        finally:
+            shutil.rmtree('/tmp/testfolder')
+
+
+    @staticmethod
+    def test_list_hidden_files():
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            Path("/tmp/testfolder/.hidden_file").touch()
+            result = subprocess.run(['ls', '-a', '/tmp/testfolder'], stdout=subprocess.PIPE)
+            print('Result: [{}]'.format(result))
+            #assert 'first.txt' in str(result.stdout), "Error while listing a folder with multiple !"
+            assert '.hidden_file' in str(result.stdout), "Error while listing hidden files in a folder !"
+        finally:
+            shutil.rmtree('/tmp/testfolder')
