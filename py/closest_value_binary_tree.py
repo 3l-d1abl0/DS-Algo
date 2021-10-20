fabc35cdaca2e8e47eda6d50e8fa711d523d5316
sameer <sameer.barha12@gmail.com>
2021-10-20 08:42:32 +0530

    script for closest value in binary tree

---
+++ py/closest_value_binary_tree.py
@@ -0,0 +1,42 @@
+
+class BinaryTree(object):
+    """docstring for BinartTree."""
+
+    def __init__(self, data):
+
+        self.key = data
+        self.left = None
+        self.right = None
+
+def closestValueInTree(root, target, closestValue):
+    if root is None:
+        #print("1. return = {}".format(closestValue))
+        return closestValue
+
+    if abs(target - root.key) < abs(target - closestValue) :
+        closestValue = root.key
+        #print("closest = {}".format(closestValue))
+
+    if target > root.key :
+        return closestValueInTree(root.right, target, closestValue)
+    elif target < root.key:
+        return closestValueInTree(root.left, target, closestValue)
+    else:
+        #print("2. return = {}".format(closestValue))
+        return closestValue
+
+if __name__ == '__main__':
+
+    root = BinaryTree(10)
+    root.left = BinaryTree(5)
+    root.right = BinaryTree(15)
+    root.left.left = BinaryTree(2)
+    root.left.right = BinaryTree(5)
+    root.right.left = BinaryTree(13)
+    root.right.right = BinaryTree(22)
+    root.left.left.left = BinaryTree(1)
+    root.right.left.right = BinaryTree(14)
+
+    #Average : O(Log(n)) time | O(Log(n)) space
+    #Worst: O(n) time | O(n) space
+    print(closestValueInTree(root, 12, float("inf")))
